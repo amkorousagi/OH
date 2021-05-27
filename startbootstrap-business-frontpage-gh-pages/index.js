54cf16dd63c6b683104ddd96da1f9129e27bb992
dkdud9261 <ayoganatanatda@naver.com>
2021-05-27 15:42:09 +0900

    Create navbar.ejs etc

--- startbootstrap-business-frontpage-gh-pages/index.js
+++ startbootstrap-business-frontpage-gh-pages/index.js
@@ -6,6 +6,10 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
+app.get('/', function(req, res) {
+    res.render('index')
+})
+
 app.get('/problem_list', function(req, res) {
     res.render('problem_list')
 })

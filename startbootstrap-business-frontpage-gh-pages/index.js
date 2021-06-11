a258cf4bf5c50b6213afe220131791e53ea3614a
Jungmin Park <pjm9562@naver.com>
2021-06-11 21:25:46 +0900

    add back to pro detail

--- startbootstrap-business-frontpage-gh-pages/index.js
+++ startbootstrap-business-frontpage-gh-pages/index.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res) {
 const getProblems = async function() {
     try {
         return await axios.get("http://localhost:3001/problem", {headers: {Authorization :"bearer " + 
-    `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGMwNWQ2YmU2YzAxZjQwZTc3NzVjZTUiLCJVc2VySWQiOiJwc2MiLCJVc2VyUGFzc3dvcmQiOiIkMmIkMTAkRGdldTBReTRNall2MHZRV0xseVM2ZXRaMHlmWjhzcFlNdFVadFdocXc5elp1TE05a1ZMZkMiLCJOaWNrbmFtZSI6ImFta29ybyIsIlNvbHZlZFByb2JsZW0iOltdLCJfX3YiOjAsImlhdCI6MTYyMzIxOTgyMSwiZXhwIjoxNjIzNDc5MDIxfQ.WG9atLITTkZwbTf1RnmAWm31At_Y8-ezGU3zRnBo6lE`}});
+    `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGMzNTNmYjhmNWM4YzMyMGE5MmUxNmMiLCJVc2VySWQiOiJwc2MiLCJVc2VyUGFzc3dvcmQiOiIkMmIkMTAkZ3dlb1FZRmZleWsxZXpsQ3VjMHZ6dVgubUtVSjJObkozZ3dyN1VLTkpxSnhKTnp2M1VtUzIiLCJOaWNrbmFtZSI6Im15bmlja25hbWUiLCJEYXRlIjoiMjAyMS0wNi0xMVQxMjoxNTo1NS41NzhaIiwiU29sdmVkUHJvYmxlbSI6W10sIl9fdiI6MCwiaWF0IjoxNjIzNDEzNzY3LCJleHAiOjE2MjM2NzI5Njd9.itQzum8hiqma7K3liyiyuwjZ4Q3owGkQldlBx1WQzm4`}});
     }catch(error) {
         console.error(error);
     }
@@ -33,6 +33,12 @@ app.get('/problem_list', async function(req, res) {
         console.log(problems[i].Rate);
     }
     res.render('problem_list', {problems});
+})
+
+app.get('/problem_detail', async function(req, res) {
+    const result = await getProblems();
+    const data = result.data.problems;
+    res.render('problem_detail', {data});
 })
 
 app.get('/community_list', function(req, res) {

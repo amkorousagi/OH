31ccbd83441b787115cc413d5a4bafc221d455f6
Jungmin Park <pjm9562@naver.com>
2021-06-12 04:33:25 +0900

    i dont know

--- startbootstrap-business-frontpage-gh-pages/index.js
+++ startbootstrap-business-frontpage-gh-pages/index.js
@@ -21,6 +21,15 @@ const getProblems = async function() {
         console.error(error);
     }
 }
+const getaProblem = async function() {
+    try {
+        return await axios.get("http://localhost:3001/problem/:id", {headers: {Authorization :"bearer " + 
+    `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGMzNTNmYjhmNWM4YzMyMGE5MmUxNmMiLCJVc2VySWQiOiJwc2MiLCJVc2VyUGFzc3dvcmQiOiIkMmIkMTAkZ3dlb1FZRmZleWsxZXpsQ3VjMHZ6dVgubUtVSjJObkozZ3dyN1VLTkpxSnhKTnp2M1VtUzIiLCJOaWNrbmFtZSI6Im15bmlja25hbWUiLCJEYXRlIjoiMjAyMS0wNi0xMVQxMjoxNTo1NS41NzhaIiwiU29sdmVkUHJvYmxlbSI6W10sIl9fdiI6MCwiaWF0IjoxNjIzNDEzNzY3LCJleHAiOjE2MjM2NzI5Njd9.itQzum8hiqma7K3liyiyuwjZ4Q3owGkQldlBx1WQzm4`}});
+    }catch(error) {
+        console.error(error);
+    }
+}
+
 
 const getUsers = async function() {
     try {
@@ -59,9 +68,10 @@ app.get('/problem_list', async function(req, res) {
     res.render('problem_list', {problems});
 })
 app.get('/problem_detail', async function(req, res) {
-    const result = await getProblems();
-    const data = result.data.problems;
-    res.render('problem_detail', {data});
+    const result = await getaProblem();
+    // const data = result.data;
+    console.log("detail"+result)
+    res.render('problem_detail');
 })
 
 app.get('/community_list', async function(req, res) {

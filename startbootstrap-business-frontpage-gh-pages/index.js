ac4d7e300b3a2f7e864d889a12593fe9c3584d7e
dkdud9261 <ayoganatanatda@naver.com>
2021-06-11 20:36:30 +0900

    modified

--- startbootstrap-business-frontpage-gh-pages/index.js
+++ startbootstrap-business-frontpage-gh-pages/index.js
@@ -22,24 +22,49 @@ const getProblems = async function() {
     }
 }
 
+const getRanks = async function() {
+    try {
+        return await axios.get("http://localhost:3001/user", {headers: {Authorization :"bearer " + 
+    `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGMwNWQ2YmU2YzAxZjQwZTc3NzVjZTUiLCJVc2VySWQiOiJwc2MiLCJVc2VyUGFzc3dvcmQiOiIkMmIkMTAkRGdldTBReTRNall2MHZRV0xseVM2ZXRaMHlmWjhzcFlNdFVadFdocXc5elp1TE05a1ZMZkMiLCJOaWNrbmFtZSI6ImFta29ybyIsIlNvbHZlZFByb2JsZW0iOltdLCJfX3YiOjAsImlhdCI6MTYyMzIxOTgyMSwiZXhwIjoxNjIzNDc5MDIxfQ.WG9atLITTkZwbTf1RnmAWm31At_Y8-ezGU3zRnBo6lE`}});
+    }catch(error) {
+        console.error(error);
+    }
+}
+
+const getPosts = async function() {
+    try {
+        return await axios.get("http://localhost:3001/post", {headers: {Authorization :"bearer " + 
+    `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGMwNWQ2YmU2YzAxZjQwZTc3NzVjZTUiLCJVc2VySWQiOiJwc2MiLCJVc2VyUGFzc3dvcmQiOiIkMmIkMTAkRGdldTBReTRNall2MHZRV0xseVM2ZXRaMHlmWjhzcFlNdFVadFdocXc5elp1TE05a1ZMZkMiLCJOaWNrbmFtZSI6ImFta29ybyIsIlNvbHZlZFByb2JsZW0iOltdLCJfX3YiOjAsImlhdCI6MTYyMzIxOTgyMSwiZXhwIjoxNjIzNDc5MDIxfQ.WG9atLITTkZwbTf1RnmAWm31At_Y8-ezGU3zRnBo6lE`}});
+    }catch(error) {
+        console.error(error);
+    }
+}
+
 app.get('/problem_list', async function(req, res) {
     const result = await getProblems();
     const data = result.data.problems;
     const problems = [];
     for(var i = 0; i < data.length; i++) {
-        var obj = new Object({Title: data[i].Title, Correct: data[i].NumOfCorrect, Submit: data[i].NumOfSubmit});
+        var obj = new Object({Title: data[i].Title, Difficulty: data[i].Difficulty, Keyword: data[i].Keyword, Correct: data[i].NumOfCorrect, Submit: data[i].NumOfSubmit});
         obj.Rate = obj.Submit ? obj.Submit/obj.Correct : 0;
         problems[i] = obj;
-        console.log(problems[i].Rate);
+        console.log(problems[i]);
     }
     res.render('problem_list', {problems});
 })
 
-app.get('/community_list', function(req, res) {
+app.get('/community_list', async function(req, res) {
+    const result = await getPosts();
+    const posts = result.data.posts;
+    console.log(posts);
     res.render('community_list')
 })
 
-app.get('/ranking', function(req, res) {
+app.get('/ranking', async function(req, res) {
+    const result = await getRanks();
+    users = result.data.user;
+    //users.sort(function(a, b) {a.SolvedProblem.length - b.SolvedProblem.length;})
+    console.log(users)
     res.render('ranking')
 })
 

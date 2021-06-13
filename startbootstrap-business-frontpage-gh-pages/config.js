7f84f3ac8ea47a3caab3b3373851a09c7c6c5e54
Ubuntu <ubuntu@ip-172-31-40-88.ap-northeast-2.compute.internal>
2021-06-13 15:20:16 +0000

    aws forever!

--- startbootstrap-business-frontpage-gh-pages/config.js
+++ startbootstrap-business-frontpage-gh-pages/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    frontURL : "13.209.22.172:4000",
-    backURL : "13.209.22.172:3001",
+	frontURL : "http://13.209.22.172:4000",
+    backURL : "http://13.209.22.172:3001",
     port : 4000
 }

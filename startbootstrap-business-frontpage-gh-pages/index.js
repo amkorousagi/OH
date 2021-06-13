4842ec9b60ce3cbff9d53588cf6110eb92b813d7
psc <hasmi5452@gmail.com>
2021-06-13 18:52:25 +0900

    no auth

--- startbootstrap-business-frontpage-gh-pages/index.js
+++ startbootstrap-business-frontpage-gh-pages/index.js
@@ -10,24 +10,24 @@ app.use(cookieParser())
 app.use(express.static("public"))
 
 app.get("/", async function (req, res) {
-  const result_problem = await axios.get("http://localhost:3001/problem", {
-  headers: { Authorization: "bearer " + req.cookies.token },
-  })
+  const result_problem = await axios.get(
+    "http://localhost:3001/problem_no_auth"
+  )
   const problems = result_problem.data.problems
-  const NumOfProblems = problems.length < 10 ? problems.length : 10;
-  const result_post = await axios.get("http://localhost:3001/post", {
-    headers: { Authorization: "bearer " + req.cookies.token },
-  })
+  const NumOfProblems = problems.length < 10 ? problems.length : 10
+  const result_post = await axios.get("http://localhost:3001/post_no_auth")
   const posts = result_post.data.posts
-  const NumOfPosts = posts.length < 10 ? posts.length : 10;
-  const result_user = await axios.get("http://localhost:3001/user/ranking", {
-    headers: { Authorization: "bearer " + req.cookies.token },
-  })
+  const NumOfPosts = posts.length < 10 ? posts.length : 10
+  const result_user = await axios.get(
+    "http://localhost:3001/user_no_auth/ranking"
+  )
   const users = result_user.data.users
-  users.sort(function(a, b) { return b.Score - a.Score})
-  const NumOfUsers = users.length < 10 ? users.length : 10;
+  users.sort(function (a, b) {
+    return b.Score - a.Score
+  })
+  const NumOfUsers = users.length < 10 ? users.length : 10
   const num = [NumOfProblems, NumOfPosts, NumOfUsers]
-  res.render("index", {problems, posts, users, num})
+  res.render("index", { problems, posts, users, num })
 })
 
 app.get("/problem_submit", async function (req, res) {
@@ -35,9 +35,7 @@ app.get("/problem_submit", async function (req, res) {
 })
 app.get("/problem_list", async function (req, res) {
   console.log(req.cookies.token)
-  const result = await axios.get("http://localhost:3001/problem", {
-    headers: { Authorization: "bearer " + req.cookies.token },
-  })
+  const result = await axios.get("http://localhost:3001/problem_no_auth")
   const problems = result.data.problems
   for (var i = 0; i < problems.length; i++) {
     problems[i].Rate = problems.NumOfSubmit
@@ -50,18 +48,15 @@ app.get("/problem_list", async function (req, res) {
 app.get("/problem_detail/:id", async function (req, res) {
   console.log(req.params.id)
   const result = await axios.get(
-    "http://localhost:3001/problem/" + req.params.id,
-    { headers: { Authorization: "bearer " + req.cookies.token } }
+    "http://localhost:3001/problem_no_auth/" + req.params.id
   )
   const data = result.data.problem
   console.log("detail " + JSON.stringify(data))
-  res.render("problem_detail", { data, pro_id:req.params.id })
+  res.render("problem_detail", { data, pro_id: req.params.id })
 })
 
 app.get("/community_list", async function (req, res) {
-  const result = await axios.get("http://localhost:3001/post", {
-    headers: { Authorization: "bearer " + req.cookies.token },
-  })
+  const result = await axios.get("http://localhost:3001/post_no_auth")
   let posts = result.data.posts
   console.log(posts)
   res.render("community_list", { posts })
@@ -73,8 +68,7 @@ app.get("/community_submit", async function (req, res) {
 app.get("/community_detail/:id", async function (req, res) {
   console.log(req.params.id)
   const result = await axios.get(
-    "http://localhost:3001/post/" + req.params.id,
-    { headers: { Authorization: "bearer " + req.cookies.token } }
+    "http://localhost:3001/post_no_auth/" + req.params.id
   )
   const data = result.data.post
   console.log(data)
@@ -83,11 +77,11 @@ app.get("/community_detail/:id", async function (req, res) {
 
 app.get("/ranking", async function (req, res) {
   console.log("rangking")
-  const result = await axios.get("http://localhost:3001/user/ranking", {
-    headers: { Authorization: "bearer " + req.cookies.token },
-  })
+  const result = await axios.get("http://localhost:3001/user_no_auth/ranking")
   users = result.data.users
-  users.sort(function(a, b) { return b.Score - a.Score})
+  users.sort(function (a, b) {
+    return b.Score - a.Score
+  })
   res.render("ranking", { users })
 })
 
@@ -105,10 +99,9 @@ app.get("/register", function (req, res) {
 
 app.get("/mypage", async function (req, res) {
   console.log("mypage")
-  const result = await axios.get(
-    "http://localhost:3001/user", {
-    headers: { Authorization: "bearer " + req.cookies.token } }
-  )
+  const result = await axios.get("http://localhost:3001/user", {
+    headers: { Authorization: "bearer " + req.cookies.token },
+  })
   const data = result.data.user
   console.log("detail " + JSON.stringify(data))
   res.render("mypage", { data })
